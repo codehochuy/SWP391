5aebc543c759e7e64dcbf11fc6e30f518def2c50
codehochuy <121989761+codehochuy@users.noreply.github.com>
2024-03-08 02:12:28 +0700

    Login Google

---
+++ src/java/Controller/LoginGoogle/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.LoginGoogle;
+
+/**
+ *
+ * @author ACER
+ */
+public class Constants {
+
+    public static String GOOGLE_CLIENT_ID = "763130706443-1ag9h8eptq9s3m17v09t55ifiklh9fi7.apps.googleusercontent.com";
+
+    public static String GOOGLE_CLIENT_SECRET = "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";
+
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/SWP391/LoginGoogleHandler";
+
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
--- web/Login.jsp
+++ web/Login.jsp
@@ -52,6 +52,8 @@
                             <div class="container-login100-form-btn">
                                 <input type="submit" value="Đăng nhập" />
                             </div>
+                            <a href="https://accounts.google.com/o/oauth2/auth?scope=email profile&redirect_uri=http://localhost:8084/SWP391/LoginGoogleHandler&response_type=code
+                               &client_id=763130706443-1ag9h8eptq9s3m17v09t55ifiklh9fi7.apps.googleusercontent.com&approval_prompt=force">Login With Google</a>
                             <div class="text-right p-t-12">
                                 <a class="txt2" href="index">
                                     Quay về trang chủ
